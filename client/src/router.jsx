ed5369d3b29c5f20e8f104a6a1c118f35776ff72
Vrishank Sharma <sharma08462@gmail.com>
2024-04-17 21:21:45 +0530

    Routing and nav completed

---
+++ client/src/router.jsx
@@ -0,0 +1,18 @@
+import { Navigate, createBrowserRouter } from "react-router-dom";
+import { PostList } from "./pages/PostList";
+import { UserList } from "./pages/UserList";
+import { TodoList } from "./pages/TodoList";
+import { RootLayout } from "./layouts/RootLayout";
+
+export const router = createBrowserRouter([
+    {
+        path:'/',
+        element: <RootLayout />,
+        children: [
+            {index:true, element: <Navigate to='/posts'/>},
+            {path: "posts", element: <PostList />},
+            {path: "users", element: <UserList/>},
+            {path: "todos", element: <TodoList />}
+        ]
+    }
+])
